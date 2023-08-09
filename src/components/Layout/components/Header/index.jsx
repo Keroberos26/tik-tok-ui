ac4d0a5fc21015f2c0fc4c0f04b2f7ea6cae326d
Keroberos26 <jinnguyen054@gmail.com>
2023-08-09 22:21:28 +0700

    Logo and Search Input

--- src/components/Layout/components/Header/index.jsx
+++ src/components/Layout/components/Header/index.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import styles from "./Header.module.scss";
+import classNames from "classnames/bind";
+import images from "~/assets/images";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleNotch,
+  faCircleXmark,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+
+const cx = classNames.bind(styles);
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="TikTok" />
+        </div>
+
+        <div className={cx("search")}>
+          <input type="search" placeholder="Tìm kiếm" />
+
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx("loading")} icon={faCircleNotch} />
+
+          <button type="submit" className={cx("search-btn")}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+
+        <div className={cx("action")}></div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
